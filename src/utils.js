a508e6fd6bbd8934ee5029773a70aee7fb9d7fdc
yiooxir <spectrs@bk.ru>
2016-07-21 13:21:33 +0300

    refactor #formatTime

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,11 @@
-export function formatTime(time){
-  const min = Math.floor(time/60)
-  const sec = Math.ceil((time-min*60))
-  return `${min}:${sec}`;
+export function formatTime(_sec){
+  const sec = Math.floor(_sec);
+  let hours   = Math.floor(sec / 3600);
+  let minutes = Math.floor((sec - (hours * 3600)) / 60);
+  let seconds = sec - (hours * 3600) - (minutes * 60);
+
+  hours < 10 &&  (hours = "0"+hours);
+  minutes < 10 && (minutes = "0"+minutes);
+  seconds < 10 && (seconds = "0"+seconds);
+  return hours+':'+minutes+':'+seconds;
 }
